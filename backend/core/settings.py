8398bd1fdd38340b7a8547e51a920e889cfa6942
YaelGuillermo <l20211785@tectijuana.edu.mx>
2024-12-07 12:16:53 -0800

    first attempt at production start-up

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -101,14 +104,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        #'ENGINE': os.environ.get('SQL_ENGINE'),
-        #'NAME': os.environ.get('SQL_DATABASE'),
-        #'USER': os.environ.get('SQL_USER'),
-        #'PASSWORD': os.environ.get('SQL_PASSWORD'),
-        #'HOST': os.environ.get('SQL_HOST'),
-        #'PORT': os.environ.get('SQL_PORT'),
+        'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
