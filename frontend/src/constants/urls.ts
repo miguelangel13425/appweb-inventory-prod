6e26a181c4f788934cc8a83e57d4c4648d87f41d
miguelangel13425 <l20211764@tectijuana.edu.mx>
2024-12-11 14:52:52 -0800

    deploy

--- frontend/src/constants/urls.ts
+++ frontend/src/constants/urls.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = '/choreo-apis/djangoreactinventory/backend/v1' // Replace with your API base URL
+export const API_BASE_URL = '/choreo-apis/inventario/backend/v1'
 export const AUTH_URL = API_BASE_URL + '/accounts/auth'
 export const ACCOUNTS_URL = API_BASE_URL + '/accounts'
 export const INVENTORY_URL = API_BASE_URL + '/inventory'
