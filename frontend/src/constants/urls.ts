908363bb1991cf42dc784721ae314e85d569ba07
YaelGuillermo <l20211785@tectijuana.edu.mx>
2024-12-10 23:13:02 -0800

    add backend url

--- frontend/src/constants/urls.ts
+++ frontend/src/constants/urls.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://127.0.0.1:8000/api/v1'
+export const API_BASE_URL = '/choreo-apis/djangoreactinventory/backend/v1' // Replace with your API base URL
 export const AUTH_URL = API_BASE_URL + '/accounts/auth'
 export const ACCOUNTS_URL = API_BASE_URL + '/accounts'
 export const INVENTORY_URL = API_BASE_URL + '/inventory'
